527b8df19902c280c1859a006ecf62dec51812f1
Alexis Algoud <alexis@atm-consulting.fr>
2014-04-03 14:28:12 +0200

    force trace

--- includes/class/class.pdo.db.php
+++ includes/class/class.pdo.db.php
@@ -56,6 +56,7 @@ function __construct($db_type = '', $connexionString='', $DB_USER='', $DB_PASS='
 		try {
 		    $this -> db = new PDO($this->connexionString, $DB_USER, $DB_PASS);
 		} catch (PDOException $e) {
+			$this->debug=true;
 		    $this->Error('PDO DB ErrorConnexion : '.$e->getMessage().' ( '. $this->connexionString.' )' );
 		} 
 	}
