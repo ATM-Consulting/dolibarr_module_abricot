1c109c6539b9b1ca90c4d75d322d853797b602ac
ATM john <john.botella@atm-consulting.fr>
2020-03-25 11:21:38 +0100

    New custom type for param

--- includes/lib/admin.lib.php
+++ includes/lib/admin.lib.php
@@ -4,21 +4,21 @@
  Copyright (C) 2013-2015 ATM Consulting <support@atm-consulting.fr>
 
  This program and all files within this directory and sub directory
- is free software: you can redistribute it and/or modify it under 
- the terms of the GNU General Public License as published by the 
- Free Software Foundation, either version 3 of the License, or any 
+ is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or any
  later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
+
  This is a library for module setup page
- 
+
  */
 
 /**
@@ -51,7 +51,7 @@ function setup_print_on_off($confkey, $title = false, $desc ='', $help = false, 
 {
     global $var, $bc, $langs, $conf, $form;
     $var=!$var;
-    
+
     print '<tr '.$bc[$var].'>';
     print '<td>';
     if(!empty($help)){
@@ -60,7 +60,7 @@ function setup_print_on_off($confkey, $title = false, $desc ='', $help = false, 
     else {
         print $title?$title:$langs->trans($confkey);
     }
-    
+
     if(!empty($desc))
     {
         print '<br><small>'.$langs->trans($desc).'</small>';
@@ -84,64 +84,71 @@ function setup_print_on_off($confkey, $title = false, $desc ='', $help = false, 
 /**
  * Auto print form part for setup
  * @param string $confkey
- * @param string $title
+ * @param bool $title
  * @param string $desc
  * @param array $metas exemple use with color array('type'=>'color') or  with placeholder array('placeholder'=>'http://')
- * @param string $type = imput or textarea
- * @param string $help
+ * @param string $type = 'imput', 'textarea' or custom html
+ * @param bool $help
+ * @param int $width
  */
 function setup_print_input_form_part($confkey, $title = false, $desc ='', $metas = array(), $type='input', $help = false, $width = 300)
 {
     global $var, $bc, $langs, $conf, $db;
     $var=!$var;
-    
+
     $form=new Form($db);
-    
+
     $defaultMetas = array(
         'name' => $confkey
     );
-    
+
     if($type!='textarea'){
         $defaultMetas['type']   = 'text';
         $defaultMetas['value']  = $conf->global->{$confkey};
     }
-    
-    
+
+
     $metas = array_merge ($defaultMetas, $metas);
     $metascompil = '';
     foreach ($metas as $key => $values)
     {
         $metascompil .= ' '.$key.'="'.$values.'" ';
     }
-    
+
     print '<tr '.$bc[$var].'>';
     print '<td>';
-    
+
     if(!empty($help)){
         print $form->textwithtooltip( ($title?$title:$langs->trans($confkey)) , $langs->trans($help),2,1,img_help(1,''));
     }
     else {
         print $title?$title:$langs->trans($confkey);
     }
-    
+
     if(!empty($desc))
     {
         print '<br><small>'.$langs->trans($desc).'</small>';
     }
-    
+
     print '</td>';
     print '<td align="center" width="20">&nbsp;</td>';
     print '<td align="right" width="'.$width.'">';
     print '<form method="POST" action="'.$_SERVER['PHP_SELF'].'" '.($metas['type'] === 'file' ? 'enctype="multipart/form-data"' : '').'>';
     print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
     print '<input type="hidden" name="action" value="set_'.$confkey.'">';
-    if($type=='textarea'){
-        print '<textarea '.$metascompil.'  >'.dol_htmlentities($conf->global->{$confkey}).'</textarea>';
-    }
-    else {
-        print '<input '.$metascompil.'  />';
-    }
-    
+
+
+		if($type=='textarea'){
+			print '<textarea '.$metascompil.'  >'.dol_htmlentities($conf->global->{$confkey}).'</textarea>';
+		}
+		elseif($type=='imput'){
+			print '<input '.$metascompil.'  />';
+		}
+		else{
+			// custom
+			print $type;
+		}
+
     print '<input type="submit" class="butAction" value="'.$langs->trans("Modify").'">';
     print '</form>';
     print '</td></tr>';
