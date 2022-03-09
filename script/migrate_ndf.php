4d204d262e7e01ba656c80b40267c463136c2f1f
John BOTELLA <john.botella@atm-consulting.fr>
2022-03-09 12:17:00 +0100

    Add paiement link

--- script/migrate_ndf.php
+++ script/migrate_ndf.php
@@ -32,7 +32,10 @@ if (! $res && file_exists("../../../../dolibarr/htdocs/main.inc.php")) $res=@inc
 if (! $res) die("Include of main fails");
 
 require_once DOL_DOCUMENT_ROOT.'/expensereport/class/expensereport.class.php';
+require_once DOL_DOCUMENT_ROOT.'/expensereport/class/paymentexpensereport.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/modules/expensereport/modules_expensereport.php';
 dol_include_once('/ndfp/class/ndfp.class.php');
+dol_include_once("/ndfp/class/ndfp.payment.class.php");
 
 /* @var $db DoliDB */
 /* @var $user User */
@@ -66,7 +69,7 @@ if(empty($conf->expensereport->enabled)) {
 
 // 2. Gestion des types de dépenses
 if(!$error) {
-    echo '2. Gestion des types de dépenses<hr>';
+	_logMsg('#'.__LINE__ . '2. Gestion des types de dépenses');
     $sql = 'INSERT IGNORE INTO ' . MAIN_DB_PREFIX . 'c_type_fees (code, label, accountancy_code, active)';
     $sql .= ' SELECT code, label, accountancy_code, active FROM ' . MAIN_DB_PREFIX . 'c_exp';
     $res = $db->query($sql);
@@ -104,10 +107,10 @@ $extrafields->addExtraField('fk_societe', 'Company', 'link', 1, 11, 'expenserepo
 // 3. Transfert des ndf
 // @TODO : Extrafield pour stocker le client concerné par la NDF afin de conserver l'info, on verra plus tard si utile ou pas
 if(!$error) {
-    echo '3. Transfert des ndf<hr>';
+	_logMsg('#'.__LINE__ . '3. Transfert des ndf', 'title');
 
     // 3.1 Récupération des types de dépenses
-    echo '3.1. Récupération des types de dépenses<hr>';
+	_logMsg('#'.__LINE__ . '3.1. Récupération des types de dépenses', 'title');
     $sql = 'SELECT id, code FROM ' . MAIN_DB_PREFIX . 'c_type_fees';
     $res = $db->query($sql);
     echo ' > ' . $sql . '<hr>';
@@ -126,7 +129,7 @@ if(!$error) {
         $sql.= ' LIMIT '.$limit;
 
         $res = $db->query($sql);
-        echo ' > ' . $sql . '<hr>';
+		_logMsg('#'.__LINE__ . $sql, 'code');
         if ($res) {
             while ($obj = $db->fetch_object($res)) {
                 $ndfp = new Ndfp($db);
@@ -141,25 +144,25 @@ if(!$error) {
 
                 if(empty($ndfp->date_valid) && ($ndfp->statut == Ndfp::STATUS_PAID || $ndfp->statut == Ndfp::STATUS_VALIDATE)) $ndfp->date_valid = $ndfp->datec;
 
-                $object = new ExpenseReport($db);
-				$object->array_options['options_fk_societe'] = $ndfp->fk_soc;
+                $expensereport = new ExpenseReport($db);
+				$expensereport->array_options['options_fk_societe'] = $ndfp->fk_soc;
 
-                $object->date_debut = $ndfp->dates;
-                $object->date_fin = $ndfp->datee;
+                $expensereport->date_debut = $ndfp->dates;
+                $expensereport->date_fin = $ndfp->datee;
 
-                $object->fk_user_author = $ndfp->fk_user;
+                $expensereport->fk_user_author = $ndfp->fk_user;
 
-                //$object->status = 1;
-                $object->fk_user_validator = $ndfp->fk_user_valid;
+                //$expensereport->status = 1;
+                $expensereport->fk_user_validator = $ndfp->fk_user_valid;
                 $desc = strtr($ndfp->description, array($ndfp->comment_admin => ''));
                 $desc = dol_concatdesc($desc, $ndfp->comment_user);
                 $desc = dol_concatdesc($desc, $ndfp->comment_admin);
-                $object->note_public = $desc;
+                $expensereport->note_public = $desc;
 
-                $id = $object->create($user, 1);
+                $id = $expensereport->create($user, 1);
                 if ($id <= 0) {
                     $error++;
-                    echo ' >> ERR : ' . $db->error() . '<hr>';
+					_logMsg('#'.__LINE__ . $db->error(), 'error');
                 }
 
                 if (!$error) {
@@ -168,7 +171,7 @@ if(!$error) {
                         $fk_type_exp = isset($aTypeExp[$line->code]) ? $aTypeExp[$line->code] : 0;
                         if(empty($line->qty)) $line->qty = 1;
                         $up = $line->total_ttc / $line->qty;
-                        $object->addline($line->qty, $up, $fk_type_exp, $line->taux, $line->dated, $line->comment, $ndfp->fk_project);
+                        $expensereport->addline($line->qty, $up, $fk_type_exp, $line->taux, $line->dated, $line->comment, $ndfp->fk_project);
                     }
                 }
 
@@ -185,54 +188,163 @@ if(!$error) {
                 $sql.= ', fk_user_approve = '.$ndfp->fk_user_valid;
                 $sql.= ', fk_statut = '.$aStatusTrans[$ndfp->statut];
                 $sql.= ', import_key = \'ndf'.$ndfp->id.'\'';
-                $sql.= ' WHERE rowid = '.$object->id;
+                $sql.= ' WHERE rowid = '.$expensereport->id;
 
                 $res2 = $db->query($sql);
-                echo ' > ' . $sql . '<hr>';
+				_logMsg('#' . __LINE__ . ' ' . $sql, 'code');
                 if (!$res2) {
                     $error++;
-                    echo ' >> ERR : ' . $db->error() . '<hr>';
+					_logMsg('#'.__LINE__ . $db->error(), 'error');
                 }
 
-                // @TODO : création du règlement + lien banque
+                // Création du règlement + lien banque
+				if(!$error && $ndfp->statut == Ndfp::STATUS_PAID){
+					$payments = $ndfp->get_payments();
+					if(is_array($payments)){
 
+						if(!empty($payments)) {
+							foreach ($payments as $payment) {
+								$ndfpPayment = new NdfpPayment($db);
+								$res = $ndfpPayment->fetch($payment->rowid);
+								if ($res) {
+									if ($payment->fk_bank) {
+										// Create a line of payments
+										$paymentExpenseReport = new PaymentExpenseReport($db);
+										$paymentExpenseReport->fk_expensereport = $expensereport->id;
+										$paymentExpenseReport->datepaid = $ndfpPayment->datepaye;
+										$paymentExpenseReport->amounts = array(// Tableau de montant
+											$ndfpPayment->fk_user_author => $ndfpPayment->amount,
+										);
+										$paymentExpenseReport->total = $ndfpPayment->amount;
+										$paymentExpenseReport->fk_typepayment = $ndfpPayment->fk_payment;
+										$paymentExpenseReport->num_payment = $ndfpPayment->num_payment;
+										$paymentExpenseReport->note_public = $ndfpPayment->note_public;
 
-                // @TODO : déplacement des fichiers du répertoire documents
-                /*
-                 * La source NDFP préhistorique : dolibarr/documents/ndfp/idDeLaNoteDeFrais
-                 * La source NDFP expense report standard : dolibarr/documents/expenseReport/idDeLaNoteDeFrais
-                 * Utiliser la fonction  pour déplacer un fichier téléchargé depuis NDFP vers expenseReport
-                 */
+										$paymentExpenseReport->fk_bank = $ndfpPayment->fk_bank;
 
+										if (!$error) {
+											$paymentExpenseReportid = $paymentExpenseReport->create($user);
+											if ($paymentExpenseReportid < 0) {
+												_logMsg('#' . __LINE__ . ' create : ' . $paymentExpenseReport->errorsToString(), 'error');
+											}
+										}
 
-                $ndfpDocumentPath = DOl_DATA_ROOT . '/ndfp/' . $ndfp->id . '/';
-                $expenseReportDocumentPath = DOl_DATA_ROOT . '/expensereport/' . $object->id . '/';
+										if (!$error) {
 
-                var_dump($ndfpDocumentPath);
-                var_dump($expenseReportDocumentPath);
-                copy($ndfpDocumentPath, $expenseReportDocumentPath);
+											// IL ne faut pas créer un nouveau paiement car déja effectué par NDFP+ il faut plutot lié ce qui à été fait
+//											$result = $paymentExpenseReport->addPaymentToBank($user, 'payment_expensereport', '(ExpenseReportPayment)', $ndfpPayment->fk_bank, '', '');
+//											if (!$result > 0) {
+//											_logMsg('#'.__LINE__ . ' addPaymentToBank : '. $paymentExpenseReport->errorsToString(), 'error');
+//											}
 
+											// Liaison avec le paiement effectué
+											$bank_line = new AccountLine($db);
+											$bank_line->fetch($ndfpPayment->fk_bank);
+
+											$acc = new Account($this->db);
+											$acc->fetch($bank_line->fk_account);
+
+											// Add link 'payment', 'payment_supplier', 'payment_expensereport' in bank_url between payment and bank transaction
+											$url = DOL_URL_ROOT . '/expensereport/payment/card.php?rowid=';
+											$result = $acc->add_url_line($bank_line->id, $expensereport->id, $url, '(paiement)', 'payment_expensereport');
+											if ($result <= 0) {
+												_logMsg('#' . __LINE__ . ' create : ' . $paymentExpenseReport->errorsToString(), 'error');
+											}
+										}
+
+//									// Logiquement pas besoin car deja fait avant
+//									if (!$error) {
+//										$paymentExpenseReport->fetch($paymentExpenseReportid);
+//										if ($expensereport->total_ttc - $paymentExpenseReport->amount == 0) {
+//											$result = $expensereport->setPaid($expensereport->id, $user);
+//											if (!$result > 0) {
+//												_logMsg('#'.__LINE__ . ' setPaid : '. $paymentExpenseReport->errorsToString(), 'error');
+//											}
+//										}
+//									}
+									}
+								} else {
+									_logMsg('#' . __LINE__ . ' Fectching NdfpPayment #' . $payment->rowid . ' fail', 'error');
+								}
+							}
+						}
+					}
+					else{
+						_logMsg('#'.__LINE__ . ' $ndfp->get_payments() #'.$ndfp->id.' fail', 'error');
+					}
+				}
+
+				if (!$error) {
+					// Déplacement des fichiers du répertoire documents
+					$ndfpDocumentPath = DOl_DATA_ROOT . '/ndfp/' . $ndfp->id;
+					$expenseReportDocumentPath = DOl_DATA_ROOT . '/expensereport/' . $expensereport->id;
+					if (is_dir($ndfpDocumentPath)) {
+
+						$logAction = 'Déplacement dossier : '
+							.str_replace(DOl_DATA_ROOT, "", $ndfpDocumentPath)
+							.' => '
+							.str_replace(DOl_DATA_ROOT, "", $expenseReportDocumentPath);
+
+						if(!is_dir($expenseReportDocumentPath) && !file_exists($expenseReportDocumentPath)){
+							_logMsg('#'.__LINE__  . $logAction);
+							rename($ndfpDocumentPath, $expenseReportDocumentPath);
+						}
+						else{
+							_logMsg('#'.__LINE__ . $logAction . ' destination already exists', 'error');
+						}
+					}
+				}
             }
         } else {
             $error++;
-            echo ' >> ERR : ' . $db->error() . '<hr>';
+			_logMsg('#'.__LINE__ . $db->error(), 'error');
         }
     } else {
         $error++;
-        echo ' >> ERR : '.$db->error().'<hr>';
+		_logMsg('#'.__LINE__ . $db->error(), 'error');
     }
 }
 
 //$error++;
 if($error > 0 || $forceRollback) {
-//    print '<br/><pre><strong><span style="background-color: red;">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;ROLLBACK !</strong></pre><br/>';
-    print '<br/><pre><strong><span style="background-color: red;">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;RollBack !</strong></pre><br/>';
+	_logMsg('#'.__LINE__ . 'RollBack !', 'error');
     $db->rollBack();
 } else {
-//    print '<br/><pre><strong><span style="background-color: green;">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;COMMIT !</strong></pre><br/>';
-    print '<br/><pre><strong><span style="background-color: green;">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;Commit !</strong></pre><br/>';
+	_logMsg('#'.__LINE__ . 'Commit !');
     $db->commit();
 }
 
 // FIN
-echo '<hr>Fin du script de migration<hr>';
+_logMsg('#'.__LINE__ . 'Fin du script de migration', 'title');
+
+/**
+ * @param $msg
+ * @param $type msg | error | title
+ * @return void
+ */
+function _logMsg($msg, $type = 'msg'){
+	global $error;
+	if(!empty($msg)){
+		if( $type == 'title'){
+			print '<h3>'.$msg.'</h3>';
+		}
+		elseif( $type == 'code'){
+			print '<pre>'.$msg.'</pre>';
+		}
+		elseif( $type == 'error'){
+			$error++;
+			print '<br/>';
+			print '<strong class="error"> >> ERR : '.$msg.'</strong>';
+		}else{
+			print '<br/>';
+			print $msg;
+		}
+	}
+}
+
+/**
+ * @return void
+ */
+function _logSep(){
+	print '<hr/>';
+}
