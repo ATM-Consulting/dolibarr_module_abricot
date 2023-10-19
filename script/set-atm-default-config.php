976d1c361225cf544a385ca551e668177c0c6ec3
atm-server <root@vps-2e2d5a30.vps.ovh.net>
2023-10-19 09:02:47 +0000

    NEW script de pré-configuration

---
+++ script/set-atm-default-config.php
@@ -0,0 +1,24 @@
+<?php
+if(is_file('../master.inc.php')) include '../master.inc.php';
+elseif(is_file('../../../master.inc.php')) include '../../../master.inc.php';
+elseif(is_file('../../../../master.inc.php')) include '../../../../master.inc.php';
+elseif(is_file('../../../../../master.inc.php')) include '../../../../../master.inc.php';
+else include '../../master.inc.php';
+
+require_once __DIR__ . '/common_script.lib.php';
+
+$BOOL_CONST_SQL_TEMPLATE = 'INSERT INTO ' . MAIN_DB_PREFIX . 'const (entity, name, value)'
+	. ' VALUES (0, \'%s\', \'%s\')'
+	. ' ON DUPLICATE KEY UPDATE value = VALUES(value);';
+
+$TCONST_TO_SET_GLOBALLY = [
+	'MAIN_USE_TOP_MENU_SEARCH_DROPDOWN' => '1',
+	'MAIN_INFO_SOCIETE_LOGO_NO_BACKGROUND' => '1',
+];
+
+foreach ($TCONST_TO_SET_GLOBALLY as $constName => $constValue) {
+	$sql = sprintf($BOOL_CONST_SQL_TEMPLATE, $constName, $constValue);
+	ast_sqlQuerylog($db, $sql);
+}
+
+echo ast_log('FIN');
